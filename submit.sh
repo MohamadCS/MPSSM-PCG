12486ada9508d5625aff1e253fc07a93e3b27096
MohamadCS <mohamadessa443@gmail.com>
2026-09-01 17:06:25 +0300

    Updated submition script

--- submit.sh
+++ submit.sh
@@ -1,2 +1,2 @@
-cp ./Paper/.aux/main.pdf paper.pdf 
-zip FinalProject -r external/neuralif results src Paper paper.pdf README.md requirements.txt benchmark_gcn.sh benchmark_preconditioner.sh run_paper_gcn.sh run_paper_preconditioner.sh
+cp ./Doc/.aux/main.pdf paper.pdf 
+zip FinalProject -r external/neuralif results src Doc paper.pdf README.md requirements.txt benchmark_gcn.sh benchmark_preconditioner.sh run_paper_gcn.sh run_paper_preconditioner.sh
